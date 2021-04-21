d11cfef9a917cda3091b4056a520a57c6055936d
root <root@Ghost.localdomain>
2021-04-21 22:24:16 +0530

    Signin functionality fixed

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import {auth, signInWithGoogle } from '../../firebase/firebase.util';
 import CustomButton from '../custombutton/custombutton';
 import FormInput from '../form-input/form-input';
 
@@ -20,9 +20,15 @@ class Signin extends Component{
         this.setState({[name]:value})
     }
 
-    handleSubmit = (event)=>{
+    handleSubmit = async event=>{
         event.preventDefault();
-        this.setState({email:'',password:''})
+        const{email,password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:''});
+        } catch(error){
+            console.error(error;)
+        }   
     }
 
     render(){
